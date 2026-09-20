e8f5cc9b6c935220890c97b4c57e8aa6b85af38b
Ninazu00 <ba.mohammad2005@gmail.com>
2026-09-20 22:18:39 +0300

    Hiding some stuff

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use('/js', express.static(path.join(__dirname, '../frontend/js')));
 // Explicit allowlist for standalone top-level pages. Must be registered
 // before the '/' catch-all below, since Express matches routes in order
 // and app.use('/', ...) would otherwise intercept every request first.
-app.get('/Plans.html', (req, res) => {
+app.get('/pl726an', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/Plans.html'));
 });
 
